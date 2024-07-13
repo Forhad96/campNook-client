cb92c696453102ab7781e3485a5675a9f70a1afb
Forhad96 <forhadairdrop@gmail.com>
2024-07-13 18:48:12 +0600

    Feat(cart):cart ui created

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,27 @@
-const Cart = () => {
-    return (
-        <div>
-            <p>HELLO I AM Cart</p>
-        </div>
-    );
+
+import Checkout from "@/components/cart/Checkout";
+import  { FC, useState } from "react";
+
+
+
+const Cart:FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const toggleCheckout = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex items-center justify-center py-8">
+      <button
+        onClick={toggleCheckout}
+        className="py-2 px-10 rounded bg-indigo-600 hover:bg-indigo-700 text-white"
+      >
+        Open Modal
+      </button>
+      <Checkout isOpen={isOpen} onClose={toggleCheckout} />
+    </div>
+  );
 };
+
 export default Cart;
