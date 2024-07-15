83b94a25cbe8aabc9224b886c3690912de5451fa
Forhad96 <forhadairdrop@gmail.com>
2024-07-15 19:40:47 +0600

    Feat(about):about ui created

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,49 @@
-const About = () => {
+import Container from "@/components/shared/Container";
+import React from "react";
+
+const About: React.FC = () => {
   return (
-    <div>
-      <h1> This is About component </h1>
+    <div id="about" className="relative bg-white overflow-hidden my-16">
+      <Container>
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100"></polygon>
+          </svg>
+
+          <div className="pt-1"></div>
+
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h2 className="my-6 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
+                About Us
+              </h2>
+
+              <p>
+                Welcome to CampNook! We're dedicated to providing camping
+                enthusiasts with high-quality gear and accessories. Our mission
+                is to enhance your outdoor adventures with products that are
+                reliable, fun, and affordable. Whether you're a seasoned camper
+                or just starting out, we have everything you need to make your
+                camping experience memorable. Thank you for choosing CampNook
+                for all your camping needs.
+              </p>
+            </div>
+          </main>
+        </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://cdn.pixabay.com/photo/2021/10/09/00/15/landscape-6692712_1280.jpg"
+          alt=""
+        />
+      </div>
+      </Container>
     </div>
   );
 };
